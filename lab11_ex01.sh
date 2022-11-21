7d5ad182f7efe21e7ae83bbd1e3a780b56cfc871
statooin <71130293+statooin@users.noreply.github.com>
2022-11-21 00:58:09 -0500

    Add files via upload

---
+++ lab11_ex01.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#
+# LL-102
+# Unit 11
+# Lab 11
+# Exercise 01
+#
+
+echo 10293847566574839201 | passwd --stdin root
+reboot
+
---
+++ lab11_ex02.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#
+# LL-102
+# Unit 11
+# Lab 11
+# Exercise 02
+#
+cp -f /bin/date /bin/mount
+reboot
+
